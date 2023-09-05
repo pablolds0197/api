e134fd180fb04660cd91541477a083a8d1e46033
Guaro2 <daguarin14@misena.edu.co>
2023-09-05 10:10:40 -0500

    Primer Commit

---
+++ git.js
@@ -0,0 +1,14 @@
+/*echo "# api" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/pablolds0197/api.git
+git push -u origin main
+*/
+
+const { init } = require("./models/usuario")
+
+
+git init
+git commint -m "primer commit "
